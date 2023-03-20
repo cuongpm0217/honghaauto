fc8aac9b79f3eaf25ce2ac4444e2c7e3feef0caa
Cuong Pham <cuongpm0217@gmail.com>
2023-03-20 08:56:01 +0700

    1st commit

---
+++ src/main/java/com/hha/HongHaAutoApplication.java
@@ -0,0 +1,13 @@
+package com.hha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongHaAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongHaAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hha/HongHaAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongHaAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
