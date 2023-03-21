a1577d092bf9884dbe465e819898cdaa1ff09854
cuongpm <cuongpm0217@gmail.com>
2023-03-21 15:08:21 +0700

    add anotation

--- src/main/java/com/hha/entities/Product.java
+++ src/main/java/com/hha/entities/Product.java
@@ -3,10 +3,14 @@ package com.hha.entities;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+@Entity
+@Data
 public class Product extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
