a070d49d263725e4b6faf8af31e3237b18451fdd
cuongpm <cuongpm0217@gmail.com>
2023-03-22 14:45:49 +0700

    content of add/delete/update/list method of BranchServiceImpl

--- src/main/java/com/hha/services/impl/BranchServiceImpl.java
+++ src/main/java/com/hha/services/impl/BranchServiceImpl.java
@@ -1,20 +1,22 @@
 package com.hha.services.impl;
 
 import java.util.List;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.hha.dtos.BranchDTO;
 import com.hha.entities.Branch;
 import com.hha.repository.BranchRepository;
 import com.hha.services.BranchService;
+
 @Service
 @Transactional
 public class BranchServiceImpl implements BranchService {
 	@Autowired
 	private BranchRepository repo;
-	
+
 	@Override
 	public Branch createBranch(Branch branch) {
 		return repo.save(branch);
@@ -23,26 +25,40 @@ public class BranchServiceImpl implements BranchService {
 	@Override
 	public Branch updateBranch(long id, Branch branch) {
 		Branch brc = repo.findById(id).get();
-		
-		return null;
+		if (Objects.nonNull(brc)) {
+			brc.setCreateDate(branch.getCreateDate());
+			brc.setGenId(branch.getGenId());
+			brc.setId(branch.getId());
+			brc.setLever(branch.getLever());
+			brc.setModifyDate(branch.getModifyDate());
+			brc.setName(branch.getName());
+			brc.setUserCreate(branch.getUserCreate());
+			brc.setUserModify(branch.getUserModify());
+		}
+		return brc;
 	}
 
 	@Override
 	public boolean deleteBranch(long id) {
-		// TODO Auto-generated method stub
-		return false;
+		repo.deleteById(id);
+		Branch brc = repo.findById(id).get();
+		boolean check=false;
+		if(!Objects.nonNull(brc)||!Objects.nonNull(brc.getName())) {
+			check=true;
+		}
+		return check;
 	}
 
 	@Override
 	public Branch getBranchById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Branch brc = repo.findById(id).get();
+		return brc;
 	}
 
 	@Override
 	public List<Branch> getAllBranch() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return (List<Branch>)repo.findAll();
 	}
 
 }
