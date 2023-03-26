9475433f6ebadc7929238180b1ddfb9280ed4356
cuongpm <cuongpm0217@gmail.com>
2023-03-27 02:33:30 +0700

    complete code

--- src/main/java/com/hha/services/impl/UserDetailsImpl.java
+++ src/main/java/com/hha/services/impl/UserDetailsImpl.java
@@ -1,51 +1,91 @@
 package com.hha.services.impl;
+
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hha.entities.User;
+
 public class UserDetailsImpl implements UserDetails {
 	private static final long serialVersionUID = 616317309785761804L;
+	private Long id;
+	private String userName;
+	private String email;
+	@JsonIgnore
+	private String passWord;
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public UserDetailsImpl(Long id, String userName, String email, String passWord,
+			Collection<? extends GrantedAuthority> authorities) {
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.passWord = passWord;
+		this.authorities = authorities;
+	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.authorities;
 	}
 
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.getPassword();
 	}
 
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.userName;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
+	}
+
+	public static UserDetails build(User user) {
+		List<GrantedAuthority> authorities = user.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+		return new UserDetailsImpl(user.getId(), user.getUserName(), user.getEmail(), user.getPassWord(), authorities);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
