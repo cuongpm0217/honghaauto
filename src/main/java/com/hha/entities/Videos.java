9eff96c1212ae88abb60d1322dba196dd7c188e0
cuongpm <cuongpm0217@gmail.com>
2023-04-01 09:59:22 +0700

    add entity-dto images, videos

---
+++ src/main/java/com/hha/entities/Videos.java
@@ -0,0 +1,65 @@
+package com.hha.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Videos extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column
+	private String tittle;
+	@Column
+	private String desciption;
+	@Column
+	private String url;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTittle() {
+		return tittle;
+	}
+	public void setTittle(String tittle) {
+		this.tittle = tittle;
+	}
+	public String getDesciption() {
+		return desciption;
+	}
+	public void setDesciption(String desciption) {
+		this.desciption = desciption;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Videos(Date createDate, Date modifyDate, long userCreate, long userModify, long branchId, boolean isEnable,
+			long id, String tittle, String desciption, String url) {
+		super(createDate, modifyDate, userCreate, userModify, branchId, isEnable);
+		this.id = id;
+		this.tittle = tittle;
+		this.desciption = desciption;
+		this.url = url;
+	}
+	public Videos(long id, String tittle, String desciption, String url) {
+		
+		this.id = id;
+		this.tittle = tittle;
+		this.desciption = desciption;
+		this.url = url;
+	}
+	public Videos() {
+		
+	}
+	
+}
